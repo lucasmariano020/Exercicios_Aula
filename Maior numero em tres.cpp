681eb47479f71a27429cef585daabe94838f19db
Lucas Jacinto Mariano <62434830+lucasmariano020@users.noreply.github.com>
2020-03-20 21:30:09 -0300

    Update Maior numero em tres.cpp

--- Maior numero em tres.cpp
+++ Maior numero em tres.cpp
@@ -1,28 +1,32 @@
 #include <stdio.h>
-int main(){
-	float a;
-	float b;
-	float c;
-	float num = 0;
+int main()
+{
+  float a;
+  float b;
+  float c;
+  float num = 0;
   
-
-	printf("Digite um numero: ");
-	scanf("%f", &a);
-	num = a;
+  printf("Digite um numero: ");
+  scanf("%f", &a);
+  num = a;
 	
-	printf("Digite outro numero: ");
-	scanf("%f", &b);
-	if(b>num){
-	num = b;
-	}
+  printf("Digite outro numero: ");
+  scanf("%f", &b);
+  
+   if(b>num)
+    {
+     num = b;
+    }
+   
+   printf("Digite outro numero: ");
+   scanf("%f",&c);
+   
+    if(c>num)
+     {
+      num = c;
+     }
 	
-	printf("Digite outro numero: ");
-	scanf("%f",&c);
-    if(c>num){
-    num = c;
-	}
-	
-	printf("O maior numero entre os digitados eh: %f",num);
-		
+  printf("O maior numero entre os digitados eh: %f",num);
+ 		
 }
 
