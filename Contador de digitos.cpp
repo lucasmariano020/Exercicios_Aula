0ec10ca484fcbd2e1a130f40b738626482e85a53
Lucas Jacinto Mariano <62434830+lucasmariano020@users.noreply.github.com>
2020-03-24 16:59:02 -0300

    Add files via upload

---
+++ Contador de digitos.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+ {
+	int num = 0;
+	int i = 0;
+	
+	printf("Digite um numero: ");
+	scanf("%d", &num);
+	
+	while(num != 0)
+	{
+	  num = (num/10);
+	  i++;
+	}
+	
+	printf("O numero tem %d digitos.", i);
+	
+ }
