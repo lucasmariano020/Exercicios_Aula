5f7d0c8c1f3ad5a1653c5ae31b3bb1f61b9435c6
Lucas Jacinto Mariano <62434830+lucasmariano020@users.noreply.github.com>
2020-03-24 22:27:57 -0300

    Update L. Numeros entre X e Y divisiveis.cpp

--- Torneio URI - 25_03/L. Numeros entre X e Y divisiveis.cpp
+++ Torneio URI - 25_03/L. Numeros entre X e Y divisiveis.cpp
@@ -1,25 +1,26 @@
 #include <stdio.h>
  
-int main() {
+int main() 
+ {
  
     int X, Y, i, j = 0;
  	scanf("%d%d", &X, &Y);
  	i = X;
  	j = Y;
  	if(X < Y)
-	{	
- 	for(i++; i < Y; i++)
- 	{
- 	  if((i % 5 == 2)||(i % 5 == 3)) printf("%d\n", i);
-	 }
-    }
-    else if(X > Y)
-    {
-	 for(j++; X > j; j++)
- 	 {
-	   if((j % 5 == 2)||(j % 5 == 3)) printf("%d\n", j);
-	 }
-    }
+	 {	
+ 	  for(i++; i < Y; i++)
+ 	   {
+ 	    if((i % 5 == 2)||(i % 5 == 3)) printf("%d\n", i);
+	   }
+         }
+        else if(X > Y)
+         {
+	  for(j++; X > j; j++)
+ 	   {
+	    if((j % 5 == 2)||(j % 5 == 3)) printf("%d\n", j);
+           }
+         }
  
     return 0;
-}
+  }
