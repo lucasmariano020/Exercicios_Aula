a2731edc1ed89ef0805a5fa5fec1528383fa6f7f
Lucas Jacinto Mariano <62434830+lucasmariano020@users.noreply.github.com>
2020-05-11 04:02:18 -0300

    Add files via upload

---
+++ Troca vogais.c
@@ -0,0 +1,49 @@
+//Troca de vogais em uma frase 
+//( 'a' pelo 'u', 'e' pelo 'o', 'i' pelo 'u', 'o' pelo 'a' e o 'u' pelo 'e')
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void trocaVogal(char str[]);
+  
+int main()
+  {
+  	char frase[100];
+  	printf("Digite a frase que tera as vogais trocadas: ");
+	fgets(frase, 100, stdin);
+	setbuf(stdin, NULL);
+	trocaVogal(frase);
+	printf("\nFrase com vogais trocadas: %s", frase);
+  }
+  
+void trocaVogal(char str[])
+  {
+  	char *pfrase;
+  	pfrase = &str[0];
+  	setbuf(stdin, NULL);
+  	int i;
+  	
+  	for(i = 0; i < strlen(str); i++)
+  	  {
+  	  	pfrase = &str[i];
+  	  	setbuf(stdin, NULL);
+  	  	switch (*pfrase)
+  	  	 {
+  	  	  case 'a': *pfrase = 'u';
+  	  	  break;
+  	  	  
+  	  	  case 'e': *pfrase = 'o';
+  	  	  break;
+  	  	  
+  	  	  case 'i': *pfrase = 'u';
+  	  	  break;
+  	  	  
+  	  	  case 'o': *pfrase = 'a';
+  	  	  break;
+  	  	  
+  	  	  case 'u': *pfrase = 'e';
+  	  	  break;
+  	     }
+	  }
+  }
