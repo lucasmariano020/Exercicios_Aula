f244506938f8b71461e887673921719082649bb0
Lucas Jacinto Mariano <62434830+lucasmariano020@users.noreply.github.com>
2020-05-12 01:35:46 -0300

    Update Calcula area e perimetro de circulo.c

--- Calcula area e perimetro de circulo.c
+++ Calcula area e perimetro de circulo.c
@@ -11,11 +11,23 @@ int main()
   	float diametro, area, *areap, perimetro, *perimetrop;
   	areap = &area;
   	perimetrop = &perimetro;
+  	
   	printf("Digite o diametro do circulo pelo qual a area e o perimetro sera calculado: ");
   	scanf("%f", &diametro);
-  	calc_circulo((diametro/2), areap, perimetrop);
-  	printf("O circulo tem area: %.2fu e perimetro: %.2fu\n", area, perimetro);
   	
+  	if(diametro >= 0)
+  	  {
+	  	calc_circulo((diametro/2), areap, perimetrop);
+	  	printf("O circulo tem area: %.2fu e perimetro: %.2fu\n", area, perimetro);
+      }
+      
+    else
+      {
+        printf("Somente numero positivo ou igual a 0");
+        return 1;
+      }
+   
+  	return 0;
   }
   
 void calc_circulo(float r, float *area, float *perimetro)
