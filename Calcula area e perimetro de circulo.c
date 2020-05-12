e88d9539830d329cda07827978a95bd83ac0de07
Lucas Jacinto Mariano <62434830+lucasmariano020@users.noreply.github.com>
2020-05-11 23:25:31 -0300

    Cálculo de área e perímetro de um círculo usando ponteiros

---
+++ Calcula area e perimetro de circulo.c
@@ -0,0 +1,25 @@
+//Calcula area e perimetro de circulo
+
+#include <stdio.h>  
+#include <stdlib.h>
+#define pi 3.14159265358979323846
+
+void calc_circulo(float r, float *area, float *perimetro);
+
+int main()
+  {
+  	float diametro, area, *areap, perimetro, *perimetrop;
+  	areap = &area;
+  	perimetrop = &perimetro;
+  	printf("Digite o diametro do circulo pelo qual a area e o perimetro sera calculado: ");
+  	scanf("%f", &diametro);
+  	calc_circulo((diametro/2), areap, perimetrop);
+  	printf("O circulo tem area: %.2fu e perimetro: %.2fu\n", area, perimetro);
+  	
+  }
+  
+void calc_circulo(float r, float *area, float *perimetro)
+  {
+    *area = (pi*r*r);
+    *perimetro = (2*pi*r);
+  }
